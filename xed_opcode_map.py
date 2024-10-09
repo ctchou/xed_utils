9fa4c2280fd6ca635f7154cf2638461507241c85
Ching-Tsun Chou <chingtsun.chou@gmail.com>
2024-10-08 17:50:00 -0700

    [SCRIPT] More work on xed_opcode_map.py

--- xed_opcode_map.py
+++ xed_opcode_map.py
@@ -5,22 +5,68 @@ import sqlite3
 from pathlib import Path
 from typing import Any, List, Tuple, Dict, Optional
 
-def one_map(map_id: int) -> str:
-    assert 0 <= map_id <= 7
+def gen_cell(map_id: int, row_id: int, col_id: int) -> str:
+    opc_int = 16 * row_id + col_id
+    opc_hex = f'{opc_int:02X}'
+    return f'''
+<td style="padding: 0px">
+<div id="cell">
+<style>
+#cell td {{
+  border: 0px none;
+  text-align: left;
+}}
+</style>
+<table style="width: 100%">
+<tr>
+<td style="width: 1%">
+<b>{opc_hex}</b>
+</td>
+<td> </td>
+</tr>
+<tr>
+<td> </td>
+<td>
+FOO
+<br>
+BAR
+</td>
+</tr>
+</table>
+</div>
+</td>
+'''
+
+def gen_row(map_id: int, row_id: int) -> str:
+    all_cols = '\n'.join([ gen_cell(map_id, row_id, col_id) for col_id in range(16) ])
+    return f'''
+<tr>
+{all_cols}
+</tr>
+'''
+
+def gen_map(map_id: int) -> str:
+    all_rows = '\n'.join([ gen_row(map_id, row_id) for row_id in range(16) ])
     return f'''
 <button class="collapsible">Map {map_id}</button>
 <div class="content">
-  <p>Map {map_id} here</p>
+<br>
+<table style="width:100%">
+{all_rows}
+</table>
+<br>
 </div>
 '''
 
-def all_maps() -> str:
+def gen_final() -> str:
+    all_maps = '\n'.join([ gen_map(map_id) for map_id in range(8) ])
     return f'''
 <!DOCTYPE html>
 <html>
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <style>
+
 .collapsible {{
   background-color: #777;
   color: white;
@@ -28,9 +74,10 @@ def all_maps() -> str:
   padding: 18px;
   width: 100%;
   border: none;
-  text-align: left;
+  text-align: center;
   outline: none;
-  font-size: 15px;
+  font-weight: bold;
+  font-size: 24px;
 }}
 
 .active, .collapsible:hover {{
@@ -54,27 +101,26 @@ def all_maps() -> str:
   max-height: 0;
   overflow: hidden;
   transition: max-height 0.2s ease-out;
-  background-color: #f1f1f1;
 }}
+
+table, th, td {{
+  border:1px solid black;
+}}
+
 </style>
 </head>
 <body>
 
-<h2>x86 opcode map</h2>
+<h1 style="text-align: center">
+x86 opcode map
+</h1>
 
 <button class="collapsible">Legend</button>
 <div class="content">
   <p>Legend here</p>
 </div>
 
-{one_map(0)}
-{one_map(1)}
-{one_map(2)}
-{one_map(3)}
-{one_map(4)}
-{one_map(5)}
-{one_map(6)}
-{one_map(7)}
+{all_maps}
 
 <script>
 var coll = document.getElementsByClassName("collapsible");
@@ -99,7 +145,7 @@ for (i = 0; i < coll.length; i++) {{
 
 def output_opcode_map(out_file: str) -> str:
     with open(out_file, 'w') as out_fp:
-        out_fp.write(all_maps())
+        out_fp.write(gen_final())
 
 def main() -> None:
     parser = argparse.ArgumentParser(description='Make HTML opcopde map from SQLite database extracted from a XED build')
