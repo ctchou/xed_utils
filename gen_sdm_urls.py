65bde7a6467f776883e13b589b709fc2c0a1fd56
Ching-Tsun Chou <chingtsun.chou@gmail.com>
2024-10-10 17:43:25 -0700

    More work on gen_sdm_urls.py

--- gen_sdm_urls.py
+++ gen_sdm_urls.py
@@ -25,7 +25,14 @@ def collect_sdm_dict() -> Dict[str, str:]:
                 sdm_dict[name] = match
         return sdm_dict
 
-sql_query = 'SELECT DISTINCT iclass from Instructions;'
+sql_query = '''
+    SELECT DISTINCT iclass from Instructions
+    WHERE extension NOT LIKE '3DNOW%'
+    AND extension NOT LIKE 'XOP%'
+    AND extension NOT LIKE 'TBM%'
+    AND extension NOT LIKE 'FMA4%'
+    AND extension NOT LIKE 'VIA_%';
+'''
 
 def collect_iclasses(db_file: str) -> List[str]:
     with sqlite3.connect(db_file) as db:
@@ -34,29 +41,71 @@ def collect_iclasses(db_file: str) -> List[str]:
         return [ inst['iclass'] for inst in insts ]
 
 cond_codes = '(o|no|b|nb|z|nz|be|nbe|s|ns|p|np|l|nl|le|nle)'
-
 re_jcc = re.compile(f'^j{cond_codes}$')
 re_setcc = re.compile(f'^set{cond_codes}$')
 re_cmovcc = re.compile(f'^cmov{cond_codes}$')
+re_fcmovcc = re.compile(r'^fcmov(b|nb|be|nbe|e|ne|u|nu)$')
 
-re_lock = re.compile(r'^(?P<stem>.+)_lock$')
-re_rep = re.compile(r'^(rep|repe|repne)_(?P<stem>.+)$')
+re_prefix_1 = re.compile(r'^(?P<stem>vpshld|vpshldv|vpshrd|vpshrdv).$')
+re_prefix_any = re.compile(r'^(?P<stem>pmovsx|pmovzx|vbroadcast|vmaskmov|vpmaskmov|vpopcnt).*$')
+re_strip_prefix = re.compile(r'^(rep|repe|repne)_(?P<stem>.+)$')
+re_strip_suffix = re.compile(r'^(?P<stem>.+)_(lock|near|far|xmm|sse4)$')
+re_strip_64 = re.compile(r'^(?P<stem>fxsave|fxrstor|pcmpestri|pcmpestrm|pcmpistri|sysret|xrstor|xrstors|xsave|xsavec|xsaveopt|xsaves)64$')
+
+misc_dict = {
+    'int': 'intn:into:int3:int1',
+    'jcxz': 'jcc',
+    'jecxz': 'jcc',
+    'jrcxz': 'jcc',
+    'loope': 'loop:loopcc',
+    'loopne': 'loop:loopcc',
+    'mov_cr': 'mov-1',
+    'mov_dr': 'mov-2',
+    'prefetcht0': 'prefetchh',
+    'prefetcht1': 'prefetchh',
+    'prefetcht2': 'prefetchh',
+    'prefetchnta': 'prefetchh',
+    'scasq': 'scas:scasb:scasw:scasd',
+    'ud0': 'ud',
+    'ud1': 'ud',
+    'ud2': 'ud',
+}
 
 def get_sdm_name(iclass: str, sdm_dict: Dict[str, str]) -> Optional[str]:
     iclass = iclass.lower()
+
     if re_jcc.match(iclass):
         return 'jcc'
     if re_setcc.match(iclass):
         return 'setcc'
     if re_cmovcc.match(iclass):
         return 'cmovcc'
-    m = re_lock.match(iclass)
+    if re_fcmovcc.match(iclass):
+        return 'fcmovcc'
+    m = re_prefix_1.match(iclass)
     if m:
-        iclass = m.group('stem')
-    m = re_rep.match(iclass)
+        return m.group('stem')
+    m = re_prefix_any.match(iclass)
     if m:
-        iclass = m.group('stem')
-    return sdm_dict.get(iclass, None)
+        return m.group('stem')
+    m = re_strip_prefix.match(iclass)
+    if m:
+        return m.group('stem')
+    m = re_strip_suffix.match(iclass)
+    if m:
+        return m.group('stem')
+    m = re_strip_64.match(iclass)
+    if m:
+        return m.group('stem')
+    name = misc_dict.get(iclass, None)
+    if name:
+        return name
+    name = sdm_dict.get(iclass, None)
+    if name:
+        return name
+    if iclass.startswith('v'):
+        return sdm_dict.get(iclass[1:], None)
+    return None
 
 def collect_sdm_urls(iclasses: List[str], sdm_dict: Dict[str, str]) -> Dict[str, str]:
     sdm_urls = dict()
